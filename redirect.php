fb54f683630e22bc867a86ba0c31ba65f23c33b3
Michael Succi <michaelsucci@gmail.com>
2012-11-28 00:12:53 -0500

    Removed application key details.

--- redirect.php
+++ redirect.php
@@ -2,9 +2,9 @@
 
 require_once 'lib/instagram.php';
 
-$client_id = 'da3f06fe4a264c8e9076eaf157798950';
-$client_secret = 'd8e6c9dfee1f4225a87030e0d0b5fb05';
-$redirect_uri = 'http://www.instagramuploader.dev/redirect.php';
+$client_id = 'GET VALUE FROM INSTAGRAM API';
+$client_secret = 'GET VALUE FROM INSTAGRAM API';
+$redirect_uri = 'GET VALUE FROM INSTAGRAM API';
 
 $uploader = new InstagramUploader($client_id, $client_secret, $redirect_uri, $_GET['code']);
 $uploader->init();
